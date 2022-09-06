02826dc7fc6f31e79a11cc1de443a44fee95acf5
battulaharish2 <96741137+battulaharish2@users.noreply.github.com>
2022-09-06 17:24:53 +0530

    Update LoadForcastLSTM.py

--- LoadForcastLSTM.py
+++ LoadForcastLSTM.py
@@ -7,19 +7,18 @@ import math
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout
 from sklearn.metrics import mean_squared_error
-# from sklearn.preprocessing import MinMaxScaler
 plt.style.use('fivethirtyeight')
 
 # Import dataset
-df = pd.read_csv(r'C:\Users\Z0172725\datasets\apartement_data\apartment\2016\Apt2_2016.csv')
+df = pd.read_csv(r'data\Apt110_2016.csv')
 df.Date = pd.to_datetime(df['Date'])
 
 # Setting the date as index will make our time series plots much more understandable.
 df.set_index('Date', inplace=True)
 
 # Normalising data
-diff = df['Load'].max() - df['Load'].min()
-df['Load'] = df['Load'] - df['Load'].min()
+# diff = df['Load'].max() - df['Load'].min()
+# df['Load'] = df['Load'] - df['Load'].min()
 
 # Firstly, we will define a new dataset equal to the existing one, but omitting the last 10 records,
 # later we will use the model to predict such values.
@@ -57,15 +56,15 @@ X_val = np.reshape(X_val, (X_val.shape[0], X_val.shape[1], 1))
 model = Sequential()
 model.add(LSTM(50, return_sequences=True, activation='relu', input_shape=(X_train.shape[1], 1), recurrent_dropout=0.2))
 model.add(LSTM(50, return_sequences=False, activation='relu'))
-model.add(Dense(150))
-model.add(Dropout(0.4))
 model.add(Dense(100))
+model.add(Dropout(0.2))
+model.add(Dense(75))
 model.add(Dropout(0.2))
 model.add(Dense(25))
 model.add(Dense(1))
-opt1 = Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999)
+opt1 = Adam(learning_rate=0.005, beta_1=0.9, beta_2=0.999)
 model.compile(loss='mean_squared_error', optimizer=opt1)
-model.fit(X_train, Y_train, epochs=30, batch_size=10, verbose=1)
+model.fit(X_train, Y_train, epochs=25, batch_size=5000, verbose=1)
 
 lstm_train_pred = model.predict(X_train)
 lstm_valid_pred = model.predict(X_val)
@@ -74,6 +73,18 @@ r2 = (np.round(np.sqrt(mean_squared_error(Y_val, lstm_valid_pred)), 2))
 print('Training error:', r1)
 print('Validation error:', r2)
 
+# MAPE calculation
+def MAPE(actual, predicted):
+    s=0
+    for i in range(len(actual)):
+        s = s + (predicted[i]-actual[i])/actual[i]
+    return s/len(actual)*100
+
+m_ap_train = MAPE(Y_train, lstm_train_pred)
+m_ap_val = MAPE(Y_val, lstm_valid_pred)
+print('MAPE_train:', m_ap_train)
+print('MAPE_valid:', m_ap_val)
+
 # Plot the validation output and prediction
 valid = pd.DataFrame(new_df[train_len:])
 valid['Predictions'] = lstm_valid_pred
@@ -81,6 +92,3 @@ plt.figure(figsize=(30, 6))
 plt.plot(valid[['Load', 'Predictions']])
 plt.legend(['Validation', 'Predictions'])
 plt.show()
-# map_val = mean_average_precision.MetricBuilder.get_metrics_list(Y_val_minmax, lstm_valid_pred)
-# map_train = mapk(Y_train_minmax, lstm_train_pred)
-# print(map_train, map_val)
